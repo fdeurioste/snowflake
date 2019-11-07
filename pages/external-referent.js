8efb4b8f2ca4fd552f6f5f97729ea687e3f1c58a
Fhenon De Urioste <fhenon.deurioste@iadvize.com>
2019-11-07 15:55:31 +0100

    feat() : Wording

--- pages/external-referent.js
+++ pages/external-referent.js
@@ -42,7 +42,7 @@ const Index = props => (
     </ul>
     <hr />
     <div>
-      <h2>Minimum Requis</h2>
+      <h2>Niveaux Attendus</h2>
       <SnowflakeApp data={data} />
     </div>
   </main>
